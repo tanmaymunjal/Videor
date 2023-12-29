dd746a864a09af57aab2e64af628d6a4670be823
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-12-29 13:13:49 +0530

    Added video transcoding api

---
+++ VideoTranscoder/transcode_video.py
@@ -0,0 +1,70 @@
+"""
+This python script transcodes all video formats
+Inspired by https://gist.github.com/hleb-albau/dced8b57fdb5d458f430
+"""
+from os import path
+from subprocess import call
+from config import (
+    FFMPEG_PATH,
+    OUTPUT_FILE_EXTENSION,
+)
+
+# example : ffmpeg -i i.mp4 -f mp4 -s 1920x1080 -b 6000k -r 30 -vcodec libx264 -preset veryslow -threads auto o.mp4
+def encode_file(
+    input_file_path,
+    output_directory,
+    output_file_extension,
+    dimension,
+    bitrate,
+    framerate,
+    preset,
+):
+    output_file_path = output_directory + compile_output_file_name(
+        input_file_path, output_file_extension, dimension, bitrate, framerate, preset
+    )
+    call(
+        [
+            FFMPEG_PATH,
+            "-i",
+            input_file_path,
+            "-f",
+            output_file_extension,
+            "-s",
+            dimension,
+            "-b:v",
+            bitrate,
+            "-r",
+            framerate,
+            "-vcodec",
+            "libx264",
+            "-preset",
+            preset,
+            "-threads",  # setting threads to 0 automatically optimises
+            "0",
+            "-strict",
+            "normal",
+            output_file_path,
+        ]
+    )
+
+
+def compile_output_file_name(
+    input_file_path, output_file_extension, dimension, bitrate, framerate, preset
+):
+    input_file_name = path.basename(input_file_path)
+    input_file_name_without_extension = path.splitext(input_file_name)[
+        0
+    ]  # split ext, not split text.
+    return (
+        input_file_name_without_extension
+        + "_"
+        + dimension
+        + "frame"
+        + framerate
+        + "bitrate"
+        + bitrate
+        + "preset"
+        + preset
+        + "."
+        + output_file_extension
+    )
---
+++ VideoTranscoder/utils.py
@@ -0,0 +1,22 @@
+import shutil
+from os import listdir, makedirs
+from os import path
+
+
+# removes all files in the directory
+def clean_directory(dir_path):
+    shutil.rmtree(dir_path, ignore_errors=True)
+    makedirs(dir_path)
+
+
+def format_input_directory_path(input_directory: str) -> str:
+    input_directory = path.join(
+        input_directory, ""
+    )  # to check if it ends in a /, and if not add the / to it.
+    return input_directory
+
+def get_files_in_input_directory(input_directory: str):
+    filesInInputDirectory = [
+        input_directory + fileName for fileName in listdir(input_directory)
+    ]
+    filesToEncode = filter(path.isfile, filesInInputDirectory)  # exclude folders
