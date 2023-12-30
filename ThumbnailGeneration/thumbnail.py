d55cf2545c0ec89342df12fffed85b92a3673c86
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-12-30 10:34:05 +0530

    Got thumbnail generation working

---
+++ ThumbnailGeneration/thumbnail.py
@@ -0,0 +1,4 @@
+import subprocess
+
+def create_image_thumbnail(video_input_path,img_output_path):
+    subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:00.000', '-vframes', '1', img_output_path])
