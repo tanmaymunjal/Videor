813f9a3d0e3f3420e45a3a05f7272b49119c95de
tanmaymunjal <tanmaymunjal64@gmail.com>
2024-01-01 07:42:26 +0000

    Added setup.sh to setup ffmpeg and terraform

---
+++ setup.sh
@@ -0,0 +1,8 @@
+// sets up ffmpeg
+sudo apt-get install ffmpeg
+
+// sets up terraform for AWS deployment
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
